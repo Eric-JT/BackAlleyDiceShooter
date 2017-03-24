13a5fef566fb558941c9009e35a9b80ac183733e
eric trotchie <Eric-JT@users.noreply.github.com>
2017-03-23 17:25:58 -0700

    option to continue or quit

--- Classes/GameHost.cs
+++ Classes/GameHost.cs
@@ -67,38 +67,6 @@ namespace DiceShooter{
             Console.Clear();
             Console.Write("Enter bet ammount: ");
         }
-
-/*
-        public static double getBetAmount(double betAmount) {
-            // Pass in input from the user
-            // returns a double
-            // check if bet is valid
-
-            // check if bet is valid
-            while (betAmount < 0 || betAmount > Player.getMoney()) {
-                // if bet is not valid inform the player and ask for bet again
-                Console.Clear();
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Invalid Bet!");
-                Console.ResetColor();
-                Thread.Sleep(2000);
-                Console.Clear();
-                Console.WriteLine("CONAN: Hey buddy, enter a valid bet.");
-                Thread.Sleep(2000);
-                Console.Clear();
-                Console.WriteLine(Player.getMoney());
-                Console.WriteLine("Bet: ");
-                Player.setBet(GameHost.getBetAmount(GameManager.checkIfNumber(Console.ReadLine())));
-            }
-
-            Console.Clear();
-            Console.WriteLine("CONAN: Thanks for your bet buddy");
-            Thread.Sleep(2000);
-            Console.Clear();
-            Console.WriteLine("CONAN: Your bet is {0}", betAmount);
-            return betAmount;
-        }*/
-
-      
+       
     }
 }
